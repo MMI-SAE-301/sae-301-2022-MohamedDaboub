07ec778e5755d42c5896830d290b5631b0bc2ef8
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-25 13:37:01 +0200

    création TABLE Montre

--- bdd.sql
+++ bdd.sql
@@ -1,10 +1,19 @@
 --
 -- code pour la création des tables
 --
-
-
-
-
+CREATE TABLE Montre(
+    id uuid  NOT NULL,
+    ecran text,
+    bracelet text,
+    boitier text,
+    Commande text,
+    id_user uuid,
+    Matériel_bracelet uuid,
+    Matériel_boitier uuid,
+    primary key (id),
+    foreign key (id_user) references auth.users(id),
+    foreign key (Matériel_boitier,Matériel_bracelet) references Matériel (code)
+);
 
 
 
