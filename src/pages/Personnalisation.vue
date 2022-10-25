fe8cb92e014c1a197e89708c84fb453da2060ee7
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-25 20:36:07 +0200

    Structeur html pour la page Personnalisation

---
+++ src/pages/Personnalisation.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1>Personnaliser</h1>  
+    <div>
+        <!-- import  -->
+    </div>  
+    <section>
+    <h2>Inspirations</h2>
+    <div>
+      <!-- fleéche gauche -->
+      <div>
+        <!-- carousel montre -->
+      </div>
+      <!-- fleéche droite -->
+    </div>
+  </section>
+  <section>
+    <h2>Les modèles les mieux notés</h2>
+    <div>
+        <img src="" alt="">
+        <div>
+            <!-- etoile -->
+        </div>
+    </div>
+    <div>
+        <img src="" alt="">
+        <div>
+            <!-- etoile -->
+        </div>
+    </div>
+    <div>
+        <img src="" alt="">
+        <div>
+            <!-- etoile -->
+        </div>
+    </div>
+    <div>
+      <RouterLink to="/Inspirations"></RouterLink>
+    </div>
+  </section>
+</template>
