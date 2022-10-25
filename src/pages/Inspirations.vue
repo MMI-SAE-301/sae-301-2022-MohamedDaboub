6a872fa87d1da922f94c66771672950af74984db
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-25 20:40:36 +0200

    Structeur de la page Inspirations

---
+++ src/pages/Inspirations.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <section>
+        <h1>Exemples de Montre</h1>
+        <div>
+            <img src="" alt="">
+            <div>
+                <!-- etoile -->
+            </div>
+            <img src="" alt="">
+            <div>
+                <!-- etoile -->
+            </div>
+            <img src="" alt="">
+            <div>
+                <!-- etoile -->
+            </div>
+        </div>
+    </section>
+</template>
