93c0e9609e7846b3ded43b66e095d1a83375a70c
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-25 15:12:24 +0200

    Structeur page Header

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,6 +2,18 @@
     <header>
         <a href="#content">Passez au contenu</a>
     </header>
+    <nav>
+        <div>
+            <RouterLink to="">Logo du Site</RouterLink>
+        </div>
+
+        <ul>
+            <li><RouterLink to=""></RouterLink></li>
+            <li><RouterLink to=""></RouterLink></li>
+            <li><RouterLink to=""></RouterLink></li>
+            <li><RouterLink to=""></RouterLink></li>
+        </ul>
+    </nav>
 </template>
 <script setup lang="ts">
 
