1aa8d7b983e6572d169da5f256a0fc368d40ebc6
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-25 18:39:17 +0200

    balise img pour l'image qui se trouve  + RouterLink vers la page Personnalisation

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,9 @@
     <h4 class="text-2xl">
       TiK•Tak le site où vous pouvez personnaliser votre montre comme vous le désirez
     </h4>
-
+    <img src="" alt=" Montre Intéligente">
+    <div  class="flex justify-center py-10" >
+    <RouterLink class="bg-Color-Bluefoncé px-8 py-4" to="/Personnalisation">Personnalisation</RouterLink>
+    </div>
   </section>
 </template>
