0f7076de24337bf20ef6374131915e3a757923a9
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-26 14:18:14 +0200

    const les router + props

--- src/components/FormMontre.vue
+++ src/components/FormMontre.vue
@@ -5,6 +5,14 @@ import { ref} from 'vue';
 import { supabase } from '@/supabase';
 import MontreSvg from './montreSvg.vue';
 import {useRouter} from 'vue-router'
+const router = useRouter()
+const props = defineProps<{
+  data?: Montre;
+  id?: string;
+}>();
+const montre = ref<Montre>(props.data ?? {});
+
+
 
 
 
