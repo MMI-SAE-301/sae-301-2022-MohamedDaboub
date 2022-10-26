d23e63b0924807c3ef28c4ec338934b6ebc9fffa
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-26 13:38:28 +0200

    mettre une taille pour tout les text de la page d'accueil

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -15,17 +15,17 @@ import montre from "../components/montreSvg.vue"
   <section>
     <div>
       <h2 class=" text-3xl md:text-4xl lg:text-5xl py-10 px-5 font-syncopate">Histoire de la marque</h2>
-      <div class="flex justify-between items-center gap-10  mx-[10%]">
-        <montre class="w-1/3"></montre>
-        <div class=" mx-10">
-          <p class="py-10 mx-10">Tik Tak est une société qui propose à ses clients d’acheter sa propre Montre Tik Tak Smart 1 totalement personnalisable. Nous offrons la possibilité de choisir la couleur de chaque composant de la montre.</p>
-          <p class="py-10 mx-10">Tik Tak  vous pousse à affirmer votre personnalité en portant les couleurs que vous souhaitez. Faites preuve d’imagination pour personnaliser vos montres qui vous ressemblent. Votre montre sera aussi unique que vous.</p>
+      <div class="flex flex-wrap md:flex-nowrap md:justify-between items-center gap-10  mx-[10%]">
+        <montre class="lg:w-2/3 w-64 "></montre>
+        <div class="mx-4 md:mx-10">
+          <p class="py-5 md:py-10 md:mx-10 lg:text-textPC">Tik Tak est une société qui propose à ses clients d’acheter sa propre Montre Tik Tak Smart 1 totalement personnalisable. Nous offrons la possibilité de choisir la couleur de chaque composant de la montre.</p>
+          <p class="py-5 md:py-10 md:mx-10 lg:text-textPC">Tik Tak  vous pousse à affirmer votre personnalité en portant les couleurs que vous souhaitez. Faites preuve d’imagination pour personnaliser vos montres qui vous ressemblent. Votre montre sera aussi unique que vous.</p>
         </div>
       </div>
     </div>
   </section>
   <section>
-    <h2 class=" text-3xl md:text-4xl lg:text-5xl py-10 px-5 font-syncopate">Inspirations</h2>
+    <h2 class=" md:text-4xl lg:text-TitrePC py-10 px-5 font-syncopate text-2xl ">Inspirations</h2>
     <div>
       <!-- fleéche gauche -->
       <div>
@@ -38,16 +38,16 @@ import montre from "../components/montreSvg.vue"
     </div>
   </section>
   <section>
-    <h2 class=" text-3xl md:text-4xl lg:text-5xl py-10 px-5 font-syncopate">Témoignages</h2>
+    <h2 class=" md:text-4xl lg:text-TitrePC py-10 px-5 font-syncopate text-2xl ">Témoignages</h2>
     <div class="flex flex-wrap gap-y-10 text-white sm:justify-center  gap-10   mx-4 md:mx-8 xl:mx-32 mb-10">
       <div class="bg-Color-Bluefoncé sm:w-1/3 rounded-xl">
         <img src="/img/imageuser1.png" class="w-1/4 my-5 rounded-full flex justify-center m-auto" alt="">
-        <p class="py-10 px-4 text-center">"Les montres sont d'excellente qualité et leur personnalisation est simple."</p>
+        <p class="py-10 px-4 text-center text-base md:text-CardText">"Les montres sont d'excellente qualité et leur personnalisation est simple."</p>
         <h3 class="py-5 px-4">- Jade Berger</h3>
       </div>
       <div class="bg-Color-Bluefoncé sm:w-1/3 rounded-xl">
         <img src="/img/imageuser2.png" class="w-1/4 my-5 rounded-full flex justify-center m-auto" alt="">
-        <p class="py-10 px-4 text-center">"La personnalisation encourage la créativité et vous permet d'avoir des Montres qui vous sont uniques." "J'adore vraiment !"</p>
+        <p class="py-10 px-4 text-center text-base md:text-CardText">"La personnalisation encourage la créativité et vous permet d'avoir des Montres qui vous sont uniques." "J'adore vraiment !"</p>
         <h3 class="py-5 px-4">- Raphaël Simon</h3>
       </div>
     </div>
