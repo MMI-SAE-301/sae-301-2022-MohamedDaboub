6bfcbcb19b631f4f3ce583a8305ceb95c03180e0
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-26 17:01:44 +0200

    appel des function dans la page personnaliser

--- src/pages/Personnalisation/index.vue
+++ src/pages/Personnalisation/index.vue
@@ -1,21 +1,39 @@
 <script setup lang="ts">
-import montre from "../../components/FormMontre.vue"
+import montreavec from "../../components/FormMontre.vue"
+import montresans from "../../components/montreSvg.vue"
+import type { Montre } from "../../types"; 
+import { supabase } from "../../supabase";
+const exemples: Montre = [{
+  ecran : "#BBE9E9",
+  bracelet : "#363553",
+  boitier_exterieure :"#363553",
+  boitier_intérieur :"#BBAE6A",
+},
+{
+  ecran : "#ffffff",
+  bracelet : "#B7B6BD",
+  boitier_exterieure :"#BBAE6A",
+  boitier_intérieur :"#BBAE6A",
+},
+{
+  ecran : "white",
+  bracelet : "#609677",
+  boitier_exterieure :"#434750",
+  boitier_intérieur :"#434750",
+},
+];
 </script>
 
 <template>
     <h1 class=" text-3xl md:text-4xl lg:text-5xl py-10 px-5 text-center font-syncopate">Personnaliser</h1>  
     <div>
-        <montre/>
+        <montreavec/>
     </div>  
     <section>
     <h2 class=" text-3xl md:text-4xl lg:text-5xl py-10 px-5">Inspirations</h2>
-    <div>
-      <!-- fleéche gauche -->
       <div>
-        <!-- carousel montre -->
+
       </div>
-      <!-- fleéche droite -->
-    </div>
   </section>
   <section>
     <h2 class=" text-3xl md:text-4xl lg:text-5xl py-10 px-5">Les modèles les mieux notés</h2>
