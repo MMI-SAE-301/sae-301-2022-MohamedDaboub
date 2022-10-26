52397dd126e490e89b338ae8204c19db290cdefa
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-26 18:58:05 +0200

    footer 60% teminé

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,60 @@
 <template>
-    
+    <footer class="bg-Color-grisfoncé pt-10 sm:mt-10">
+    <div class="max-w-7xl m-auto  flex flex-wrap justify-left text-white">
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            <div class="xl:text-2xl text-lg uppercase  font-bold mb-6">
+                LIEN
+            </div>
+                <a href="#" class="my-3 block  hover:text-gray-100 lg:text-xl text-base lg:mx-4 mx-2 font-normal duration-700">
+                    Personnalisation
+                </a>
+                <a href="#" class="my-3 block  hover:text-gray-100 lg:text-xl text-base lg:mx-4 mx-2  font-normal duration-700">
+                    Inspirations
+                </a>
+                <a href="#" class="my-3 block  hover:text-gray-100 lg:text-xl text-base lg:mx-4 mx-2  font-normal duration-700">
+                    À propos
+                </a>
+                <a href="#" class="my-3 block  hover:text-gray-100 lg:text-xl text-base lg:mx-4 mx-2  font-normal duration-700">
+                    Votre sélection
+                </a>
+        </div>
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            <div class="xl:text-2xl text-lg uppercase  font-bold mb-6">
+                INFORMATION
+            </div>
+                <a href="#" class="my-3 block  hover:text-gray-100 lg:text-xl text-base lg:mx-4 mx-2  font-normal duration-700">
+                    Mentions légales
+                </a>
+                <a href="#" class="my-3 block  hover:text-gray-100 lg:text-xl text-base lg:mx-4 mx-2  font-normal duration-700">
+                    Gestion des cookies
+                </a>
+                <a href="#" class="my-3 block  hover:text-gray-100 lg:text-xl text-base lg:mx-4 mx-2  font-normal duration-700">
+                    CGU/CGV
+                </a>
+        </div>
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            <div class="xl:text-2xl text-lg uppercase  font-bold mb-6">
+                SUIVEZ-NOUS
+            </div>  
+        </div>
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            <div class="uppercase xl:text-2xl text-lg   font-bold mb-6">
+                Où nous trouver
+            </div>
+        </div>
+            
+    </div>
+    <div class="pt-2">
+        <div class="flex pb-5 px-3 m-auto pt-5 border-t border-t-Color-vertfoncé  bord text-white text-base  flex-col md:flex-row  lg:flex lg:justify-between sm:justify-center ">
+            <div class="mt-2 text-center px-10">
+                ©2022 TiK•TaK. All rights reserved
+            </div>
+            <div class="justify-center ">
+                <p>Projet réalisé dans le cadre d’un exercice pédagogique au <a class="border-b-2 border-solid border-white" href=""> département MMi de Montbéliard. </a></p>
+            </div>
+        </div>
+    </div>
+</footer>
 </template>
 <script setup lang="ts">
 </script>
