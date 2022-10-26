74a21055a55e16ca82e2160b56d6b0ae9f120307
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-26 18:58:18 +0200

    appel dans app vue

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,11 @@
   <Suspense>
     <router-view id="content"  />
   </Suspense>
+  <footer1/>
 </template>
 
 <script setup lang="ts">
 import header1 from "./components/header.vue";
+import footer1 from "./components/Footer.vue"
 import { Bars3Icon } from "@heroicons/vue/20/solid";
 </script>
