6f8d1e0a3c73698fac98d7c9efaaf504ec38c681
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-26 14:08:09 +0200

    les choix des couleurs

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,14 @@ export interface Montre {
   Boitier_exterieure?: string;
   Boitier_intérieur?: string;
 }
+export const colors = {
+  "#363553": "Bleu foncé",
+  "#FFFFFF": "Blanc",
+  "#283D31": "Vert foncé",
+  "#262222": "gris",
+  "#BBAE6A": "or",
+  "#896D79": "Rose foncé",
+  "#A2A2AD": "Silver",
+  "#434750": "Acier",
+  "#1DA1F2": "Bleu",
+};
