075eb2af0e785a63e9d12cbc01e593834e6e658d
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-26 11:00:12 +0200

    page inspiration terminer

--- src/pages/Inspirations.vue
+++ src/pages/Inspirations.vue
@@ -7,7 +7,7 @@ import star from "../components/Icons/Star.vue"
         <h1 class=" text-3xl md:text-4xl lg:text-5xl py-10 px-5 ">Exemples de Montre</h1>
         <div class=" grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-10 mx-5 md:mx-10">
             <div class="bg-Color-Bluefoncé rounded-xl" >
-                <img src="/public/img/Montre1.png" class="flex justify-center m-auto" alt="">
+                <img src="/public/img/Montre1.png" class="flex justify-center m-auto " alt="">
                 <div class="flex md:gap-6 gap-2  lg:gap-3 py-4  justify-center ">
                     <star class=" w-8 md:w-6  lg:w-12"></star>
                     <star class=" w-8 md:w-6  lg:w-12"></star>
@@ -16,16 +16,24 @@ import star from "../components/Icons/Star.vue"
                     <star class=" w-8 md:w-6  lg:w-12"></star>
                 </div>
             </div>
-            <div>
-                <img src="/public/img/Montre2.png" alt="">
-                <div>
-                    <!-- etoile -->
+            <div class="bg-Color-Bluefoncé rounded-xl" >
+                <img src="/public/img/Montre2.png" class="flex justify-center m-auto sm:h-[385px]" alt="">
+                <div class="flex md:gap-6 gap-2  lg:gap-3 py-4  justify-center ">
+                    <star class=" w-8 md:w-6  lg:w-12"></star>
+                    <star class=" w-8 md:w-6  lg:w-12"></star>
+                    <star class=" w-8 md:w-6  lg:w-12"></star>
+                    <star class=" w-8 md:w-6  lg:w-12"></star>
+                    <star class=" w-8 md:w-6  lg:w-12"></star>
                 </div>
             </div>
-            <div>
-                <img src="/public/img/Montre3.png" alt="">
-                <div>
-                    <!-- etoile -->
+            <div class="bg-Color-Bluefoncé rounded-xl" >
+                <img src="/public/img/Montre3.png" class="flex justify-center m-auto sm:h-[385px] " alt="">
+                <div class="flex md:gap-6 gap-2  lg:gap-3 py-4  justify-center ">
+                    <star class=" w-8 md:w-6  lg:w-12"></star>
+                    <star class=" w-8 md:w-6  lg:w-12"></star>
+                    <star class=" w-8 md:w-6  lg:w-12"></star>
+                    <star class=" w-8 md:w-6  lg:w-12"></star>
+                    <star class=" w-8 md:w-6  lg:w-12"></star>
                 </div>
             </div>
         </div>
