363439973bf06f7a06a1a0e20805837fc005b6e6
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-26 21:52:37 +0200

    fun headlessui good

--- src/components/FormMontre.vue
+++ src/components/FormMontre.vue
@@ -6,6 +6,23 @@ import { supabase } from '@/supabase';
 import MontreSvg from './montreSvg.vue';
 import formkitlistecolor from "./FormKitListColors.vue"
 import {useRouter} from 'vue-router'
+
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from '@headlessui/vue'
+
+const isOpen = ref(false)
+
+function closeModal() {
+  isOpen.value = false
+}
+function openModal() {
+  isOpen.value = true
+}
 const router = useRouter()
 const props = defineProps<{
   data?: Montre;
@@ -18,7 +35,7 @@ async function upsertMontre(dataForm, node) {
   if (error) node.setErrors([error.message]);
   else {
     node.setErrors([]);
-    router.push({ name: "Personnalisation-edit-id", params: { id: data[0].id } });
+    router.push({ name: "", params: { id: data[0].id } });
   }
 }
 if (props.id) {
@@ -30,6 +47,7 @@ if (props.id) {
     console.log("n'a pas pu charger le table Montre :", error);
   else montre.value = data[0];
 }
+
 </script>
 <template>
     <div class="flex flex-wrap justify-between md:justify-center  md:gap-20 ">
@@ -41,7 +59,7 @@ if (props.id) {
         </div>
         <div class="">
             <h2 class="text-2xl   pb-10">Couleur </h2>
-            <FormKit type="form" v-model="montre" submit-class="bg-Color-Bluefoncé px-8 rounded-lg py-4 text-white" submit-label="Enregistrer votre Montre"   @submit="upsertMontre" :submit-attrs="{ classes: { input: 'bg-Color-Bluefoncé px-8 rounded-lg py-4 text-white '} }"  >
+            <FormKit type="form" v-model="montre" submit-class="bg-Color-Bluefoncé px-8 rounded-lg py-4 text-white" submit-label="Enregistrer votre Montre"   @submit="openModal"  :submit-attrs="{ classes: { input: 'bg-Color-Bluefoncé px-8 rounded-lg py-4 text-white '} }"  >
                 <FormKit name="ecran" label="ecran" value="#FFFFFF" type="radio" :options="colorsecran" :sections-schema="{inner:{$el:null},decorator:{sel:null}}" input-class="peer sr-only" options-class="flex gap-10"><template #label="context"><div class="h-6 w-6 rounded-full border-2 peer-checked:border-red-600" :style="{backgroundColor:context.option.value}"><span class="sr-only">{{context.option.label}}</span></div></template></FormKit>
                 <formkitlistecolor name="bracelet" label="bracelet"/>
                 <formkitlistecolor name="boitier_exterieure" label="boitier_exterieure"/>
@@ -61,6 +79,63 @@ if (props.id) {
             </FormKit> -->
 
         </div>
+        <TransitionRoot appear :show="isOpen" as="template">
+          <Dialog as="div" @close="closeModal" class="relative z-10 ">
+            <TransitionChild
+              as="template"
+              enter="duration-300 ease-out"
+              enter-from="opacity-0"
+              enter-to="opacity-100"
+              leave="duration-200 ease-in"
+              leave-from="opacity-100"
+              leave-to="opacity-0"
+            >
+              <div class="fixed inset-0 bg-black bg-opacity-25" />
+            </TransitionChild>
+
+            <div class="fixed inset-0 overflow-y-auto">
+              <div
+                class="flex min-h-full items-center justify-center p-4 text-center"
+              >
+                <TransitionChild
+                  as="template"
+                  enter="duration-300 ease-out"
+                  enter-from="opacity-0 scale-95"
+                  enter-to="opacity-100 scale-100"
+                  leave="duration-200 ease-in"
+                  leave-from="opacity-100 scale-100"
+                  leave-to="opacity-0 scale-95"
+                >
+                  <DialogPanel
+                    class="w-full max-w-md transform overflow-hidden rounded-2xl bg-Color-Bluefoncé p-6 text-left align-middle shadow-xl transition-all"
+                  >
+                    <div class="mt-2">
+                      <p class="text-xl text-white text-center py-5 ">
+                        Votre montre a bien été enregistrée.
+                      </p>
+                    </div>
+
+                    <div class="mt-4 flex gap-4">
+                      <RouterLink to="/vosModèles"
+                        type="button"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-Color-vertfoncé px-4 py-2 text-sm font-medium text-white  focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      >
+                      Voir tout mes Model
+                      </RouterLink>
+                      <p class="text-white">Ou</p>
+                      <RouterLink to="/Payement"
+                        type="button"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-Color-vertfoncé px-4 py-2 text-sm font-medium text-white  focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      >
+                      Passer votre commande
+                      </RouterLink>
+                    </div>
+                  </DialogPanel>
+                </TransitionChild>
+              </div>
+            </div>
+          </Dialog>
+        </TransitionRoot>
 
 
     </div>
