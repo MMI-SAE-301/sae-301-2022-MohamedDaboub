2af8e818faaf51b7a05483656fdd2bd7bd1f5258
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-26 10:50:16 +0200

    css card montre dans le inspiration

--- src/pages/Inspirations.vue
+++ src/pages/Inspirations.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
-
+import star from "../components/Icons/Star.vue"
 </script>
 
 <template>
     <section>
         <h1 class=" text-3xl md:text-4xl lg:text-5xl py-10 px-5 ">Exemples de Montre</h1>
-        <div class=" grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(285px,1fr))] gap-8">
-            <div >
-                <img src="/public/img/Montre1.png" alt="">
-                <div>
-                    <!-- etoile -->
+        <div class=" grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-10 mx-5 md:mx-10">
+            <div class="bg-Color-Bluefoncé rounded-xl" >
+                <img src="/public/img/Montre1.png" class="flex justify-center m-auto" alt="">
+                <div class="flex md:gap-6 gap-2  lg:gap-3 py-4  justify-center ">
+                    <star class=" w-8 md:w-6  lg:w-12"></star>
+                    <star class=" w-8 md:w-6  lg:w-12"></star>
+                    <star class=" w-8 md:w-6  lg:w-12"></star>
+                    <star class=" w-8 md:w-6  lg:w-12"></star>
+                    <star class=" w-8 md:w-6  lg:w-12"></star>
                 </div>
             </div>
             <div>
