eef2c8bd6b055752e4306e073ba4502e109ed657
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-26 14:17:09 +0200

    import des element pour faire formMontre

---
+++ src/components/FormMontre.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { Montre, } from '@/types';
+import { colors, } from '@/types';
+import { ref} from 'vue';
+import { supabase } from '@/supabase';
+import MontreSvg from './montreSvg.vue';
+import {useRouter} from 'vue-router'
+
+
+
+
+</script>
