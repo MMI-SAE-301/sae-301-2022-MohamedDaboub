10164a00b8e91eb4551749b35a26691eedf443a5
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-10-26 17:12:54 +0200

    button pour Voir Plus

--- src/pages/Personnalisation/index.vue
+++ src/pages/Personnalisation/index.vue
@@ -44,39 +44,39 @@ const exemples: Montre = [{
   <section>
     <h2 class=" text-3xl md:text-4xl lg:text-5xl py-10 px-5">Les modèles les mieux notés</h2>
     <div class=" grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-10 mx-5 md:mx-10 my-10">
-            <div class="bg-Color-Bluefoncé rounded-xl" >
-                <img src="/public/img/Montre1.png" class="flex justify-center m-auto " alt="">
-                <div class="flex md:gap-6 gap-2  lg:gap-3 py-4  justify-center ">
-                    <star class=" w-8 md:w-6  lg:w-12"></star>
-                    <star class=" w-8 md:w-6  lg:w-12"></star>
-                    <star class=" w-8 md:w-6  lg:w-12"></star>
-                    <star class=" w-8 md:w-6  lg:w-12"></star>
-                    <starvide class=" w-8 md:w-6  lg:w-12 "></starvide>
-                </div>
-            </div>
-            <div class="bg-Color-Bluefoncé rounded-xl" >
-                <img src="/public/img/Montre2.png" class="flex justify-center m-auto sm:h-[385px]" alt="">
-                <div class="flex md:gap-6 gap-2  lg:gap-3 py-4  justify-center ">
-                    <star class=" w-8 md:w-6  lg:w-12"></star>
-                    <star class=" w-8 md:w-6  lg:w-12"></star>
-                    <star class=" w-8 md:w-6  lg:w-12"></star>
-                    <star class=" w-8 md:w-6  lg:w-12"></star>
-                    <starvide class=" w-8 md:w-6  lg:w-12"></starvide>
-                </div>
-            </div>
-            <div class="bg-Color-Bluefoncé rounded-xl" >
-                <img src="/public/img/Montre3.png" class="flex justify-center m-auto sm:h-[385px] " alt="">
-                <div class="flex md:gap-6 gap-2  lg:gap-3 py-4  justify-center ">
-                    <star class=" w-8 md:w-6  lg:w-12"></star>
-                    <star class=" w-8 md:w-6  lg:w-12"></star>
-                    <star class=" w-8 md:w-6  lg:w-12"></star>
-                    <star class=" w-8 md:w-6  lg:w-12"></star>
-                    <starvide class=" w-8 md:w-6  lg:w-12"></starvide>
-                </div>
-            </div>
-        </div>
-    <div>
-      <RouterLink to="/Inspirations"></RouterLink>
+      <div class="bg-Color-Bluefoncé rounded-xl" >
+          <img src="/public/img/Montre1.png" class="flex justify-center m-auto " alt="">
+          <div class="flex md:gap-6 gap-2  lg:gap-3 py-4  justify-center ">
+              <star class=" w-8 md:w-6  lg:w-12"></star>
+              <star class=" w-8 md:w-6  lg:w-12"></star>
+              <star class=" w-8 md:w-6  lg:w-12"></star>
+              <star class=" w-8 md:w-6  lg:w-12"></star>
+              <starvide class=" w-8 md:w-6  lg:w-12 "></starvide>
+          </div>
+      </div>
+      <div class="bg-Color-Bluefoncé rounded-xl" >
+          <img src="/public/img/Montre2.png" class="flex justify-center m-auto sm:h-[385px]" alt="">
+          <div class="flex md:gap-6 gap-2  lg:gap-3 py-4  justify-center ">
+              <star class=" w-8 md:w-6  lg:w-12"></star>
+              <star class=" w-8 md:w-6  lg:w-12"></star>
+              <star class=" w-8 md:w-6  lg:w-12"></star>
+              <star class=" w-8 md:w-6  lg:w-12"></star>
+              <starvide class=" w-8 md:w-6  lg:w-12"></starvide>
+          </div>
+      </div>
+      <div class="bg-Color-Bluefoncé rounded-xl" >
+          <img src="/public/img/Montre3.png" class="flex justify-center m-auto sm:h-[385px] " alt="">
+          <div class="flex md:gap-6 gap-2  lg:gap-3 py-4  justify-center ">
+              <star class=" w-8 md:w-6  lg:w-12"></star>
+              <star class=" w-8 md:w-6  lg:w-12"></star>
+              <star class=" w-8 md:w-6  lg:w-12"></star>
+              <star class=" w-8 md:w-6  lg:w-12"></star>
+              <starvide class=" w-8 md:w-6  lg:w-12"></starvide>
+          </div>
+      </div>
     </div>
+      <div class="flex justify-center my-10 ">
+        <RouterLink class="bg-Color-Bluefoncé px-8 rounded-lg py-4 text-white " to="/Inspirations">Voir Plus</RouterLink>
+      </div>
   </section>
 </template>
